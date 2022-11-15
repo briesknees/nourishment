ac1ab740b4e6839ef7a852e5553c1f5a8401cc17
Drew Dunne <drewdunne88@gmail.com>
2022-11-15 13:13:15 -0800

    Added initial types for API

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,34 +1,49 @@
-
-// Example 'nextReset' value: 1668542399000; This value will be acquired from DB
-
 /**
- * Set on server during 'Update' event.
+ * Updated on each each Task whenever a "Cycle" ends.
+ * @remarks Generally, a cycle's end triggers all tasks to degrade one status. 
+ * Watering can reverse this
+ * @privateRemarks Cycles will be hard-coded to once per day at 11:59pm for the time being.
  */
 export enum Status { 
-  Excess, // Did extra in a day
-  Great, // 
-  Good, // 
-  Poor, // 
-  Bad // Neglected
+  Excess = 'Excess', // Did extra in a Cycle
+  Great = 'Great', // Did the expected number of times this cycle
+  Good = 'Good', // Ended 'Great' last cycle
+  Poor = 'Poor', // Ended 'Good' last cycle
+  Bad = 'Bad' // Ended 'Poor' last cycle
 }
 
-export enum PlantModel { Flower = 'flower', Cactus = 'cactus' }
-export enum Frequency { 
-  Weekly = 604800000, 
-  Daily = 86400000, 
-  Hours12 = 43200000  
-}
+/**
+ * Visual Models for plants
+ * @values Flower or Cactus
+ */
+export enum PlantModel { Flower = 'Flower', Cactus = 'Cactus' }
 
+/**
+ * Acceptable frequencies for tasks to be completed.
+ * @values Weekly, Daily, or Hours12
+ */
+export enum Frequency { Weekly = 'Weekly', Daily ='Daily', Hours12 ='2x Daily'}
+
+/**
+ * Data related to the user which should be retrieved by the front-end upon app initialization.
+ */
 export type userData = {
+  id: string,
   tasks: Task[],
+  points: number,
+  currentStreak: number,
+  longestStreak: number,
 }
 
+/**
+ * Holds the core information related to user Tasks.
+ */
 export type Task = {
   id: string,
   name: string,
   model: PlantModel,
   status: Status,
-  frequency: Frequency,
+  Frequency: Frequency,
   actionCount: number,
 }
 
