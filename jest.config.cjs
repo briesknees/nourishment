3e2aff2c4fab919f490b8d9c4788ca1c85f5014a
forestleigh <theforestleigh@gmail.com>
2022-11-15 14:24:45 -0800

    7 out of 7 tests pass

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,6 +6,6 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
       '<rootDir>/src/client/__mocks__/fileMock.ts',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/client/__tests__/setupTest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/client/__mocks__/setupTest.ts'],
   "verbose": true
 };
