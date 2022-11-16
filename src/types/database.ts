01ee8260ba8caf1904386d03921c30a1e1f29f8e
Drew Dunne <drewdunne88@gmail.com>
2022-11-15 22:07:31 -0800

    Updated Database Types

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,5 +1,14 @@
-export type InsertQuery = {
+export interface SelectQuery {
+  table: string;
+  columns: string[];
+  conditions?: {
+    where?: string;
+    //...
+  };
+}
+export interface InsertQuery {
   table: string;
   columns: string[];
   values: string[];
 };
+
